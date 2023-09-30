3a968edd593def7a036147bd3258f7e88d79532e
Daniele Muffato <danixgithub1@gmail.com>
2023-09-30 14:24:49 +0200

    added htop to automatically downloaded packages

--- init/apt-packages.sh
+++ init/apt-packages.sh
@@ -10,7 +10,7 @@ done
 while ! sudo apt upgrade -y; do
     color "1;31" "upgrade failed"
 done
-while ! sudo apt install tree ripgrep fzf zoxide bat vim unclutter-xfixes xsel neofetch -y; do
+while ! sudo apt install tree ripgrep fzf zoxide bat vim unclutter-xfixes xsel neofetch htop -y; do
     color "1;31" "installation of utilities failed!"
 done
 while ! sudo apt install deluge code fonts-firacode gnome-tweaks gnome-shell-extension-manager -y; do
