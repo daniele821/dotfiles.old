daniele821/dotfiles.old

commit 5ccb07d0f12b91116f0e9c2c461053982c702074

files_to_track is now $USER indipendent

autosaver.sh
@@ -121,6 +121,7 @@ function check_git_user() {
 # (2.7) read lines from file
 function read_files() {
     while read -r file || [[ -n "${file}" ]]; do
+        [[ -n "${file}" && "${file:0:1}" != "#" ]] && file="${HOME}/${file}"
         if ! git -C "${SCRIPT_DIR}" check-ignore -q "${DIRS[0]}/${file}"; then
             if [[ -f "${file}" ]] || [[ -f "${DIRS[0]}${file}" ]]; then
                 echo "${file}"

commit 6a3e7db64277b9c90bbc5f73e9cf4e91e1e91fae

fixed bug [EVERY FILE IN BACKUP DIR WOULD BE GET BY READ_FILES FUNCTION]. Caused by commit 5ccb07d

autosaver.sh
@@ -123,13 +123,13 @@ function read_files() {
     while read -r file || [[ -n "${file}" ]]; do
         [[ -n "${file}" && "${file:0:1}" != "#" ]] && file="${HOME}/${file}"
         if ! git -C "${SCRIPT_DIR}" check-ignore -q "${DIRS[0]}/${file}"; then
-            if [[ -f "${file}" ]] || [[ -f "${DIRS[0]}${file}" ]]; then
+            if [[ -f "${file}" || -f "${DIRS[0]}${file}" ]]; then
                 echo "${file}"
             fi
             if [[ -d "${file}" ]]; then
                 find "${file}" -type f
             fi
-            if [[ -d "${DIRS[0]}${file}" ]]; then
+            if [[ -n "${file}" && -d "${DIRS[0]}${file}" ]]; then
                 find "${DIRS[0]}${file}" -type f | while read -r tmp; do
                     echo "${tmp:${#DIRS[0]}}"
                 done