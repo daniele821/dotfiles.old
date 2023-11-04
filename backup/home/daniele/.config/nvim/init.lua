5a3cd78052d525e7a77cbd5aa2bd89715af376da
Daniele Muffato <danixgithub1@gmail.com>
2023-11-04 19:41:28 +0100

    changed neovim package manager (packer ---> lazy) [TODO: lsp config]

--- backup/home/daniele/.config/nvim/init.lua
+++ backup/home/daniele/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- vim config
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.hlsearch = false
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -9,56 +10,107 @@ vim.opt.breakindent = true
 vim.opt.signcolumn = 'yes'
 vim.opt.showmode = false
 vim.opt.scrolloff = 5
+vim.opt.completeopt = 'menuone,noselect'
+vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.cmd [[ 
+vim.cmd [[
     autocmd BufEnter * set formatoptions-=cro
-    autocmd BufEnter * setlocal formatoptions-=cro 
+    autocmd BufEnter * setlocal formatoptions-=cro
 ]]
 
+
 -- keybindings config
-vim.keymap.set('t','<ESC>','<C-\\><C-n>',{})                -- exit terminal mode with 
-vim.keymap.set('n','<Leader>','<NOP>',{})                   -- disable leader key in normal mode
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', {}) -- exit terminal mode with
+vim.keymap.set({'n','v'}, '<Space>', '<NOP>', {})    -- disable leader key in normal mode
 
--- plugin configs 
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+
+-- lazy bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-local packer_bootstrap = ensure_packer()
-return require('packer').startup(function(use)
-    -- plugins declaration
-    use 'wbthomason/packer.nvim'
-    use 'navarasu/onedark.nvim'
-    use 'nvim-lualine/lualine.nvim'
-    use 'lewis6991/gitsigns.nvim'
-    use 'numToStr/Comment.nvim'
-    use {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x', 
-        requires = { 'nvim-lua/plenary.nvim' }
-    }
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        build = ':TSUpdate',
-    } 
+vim.opt.rtp:prepend(lazypath)
 
-    -- plugins setup
-    require('onedark').setup {
-        style = 'dark',
-    }
-    require('onedark').load()
-    require('lualine').setup()
-    require('gitsigns').setup()
-    require('Comment').setup()
+
+-- plugins config
+require('lazy').setup{
+    -- one-dark theme
+    {
+        'navarasu/onedark.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            style = 'darker'
+            vim.cmd.colorscheme 'onedark'
+        end
+    },
+
+    -- git-signs
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {},
+        lazy = false,
+    },
+
+    -- status-line 
+    {
+        'nvim-lualine/lualine.nvim',
+        opts = {},
+        lazy = false,
+    },
+    
+    -- comments
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+        lazy = false,
+    },
+
+    -- telescope
+    {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.4',
+        lazy = false,
+        dependencies = { 
+            'nvim-lua/plenary.nvim',
+            lazy = true,
+        }
+    },
+
+    -- tree-sitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            lazy = false,
+        },
+    },
+}
+
+
+-- plugin keybindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+
+
+-- config tree-sitter 
+vim.defer_fn(function()
     require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'toml', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
@@ -66,22 +118,17 @@ return require('packer').startup(function(use)
             enable = true,
         },
     }
-
-    -- plugin keybindings
-    local builtin = require('telescope.builtin')    
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})   -- telescope find files
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})    -- telescope grep files
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})      -- telescope find buffers
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})    -- telescope neovim help
-
-    -- packer bootstrap
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+end, 0)
 
 
-
-
+-- highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
 
 
