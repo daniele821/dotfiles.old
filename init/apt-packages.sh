d256c4833cd207c20b868052093c885c5671f215
Daniele Muffato <danixgithub1@gmail.com>
2023-11-07 00:34:38 +0100

    added automagical download of papirus icon theme

--- init/apt-packages.sh
+++ init/apt-packages.sh
@@ -13,10 +13,13 @@ done
 while ! sudo apt-add-repository ppa:neovim-ppa/unstable -y; do
     color "1;31" "adding neovim instable ppa failed"
 done
+while ! sudo apt-add-repository ppa:papirus/papirus -y; do
+    color "1;31" "adding neovim instable ppa failed"
+done
 while ! sudo apt update -y; do
     color "1;31" "update failed"
 done
-while ! sudo apt install tree ripgrep fzf zoxide bat vim neovim unclutter-xfixes xsel neofetch htop -y; do
+while ! sudo apt install tree ripgrep fzf zoxide bat vim neovim unclutter-xfixes xsel neofetch htop papirus-icon-theme -y; do
     color "1;31" "installation of utilities failed!"
 done
 while ! sudo apt install deluge code fonts-firacode gnome-tweaks gnome-shell-extension-manager -y; do
