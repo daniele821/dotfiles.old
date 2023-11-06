55d144dcccbb8352918f72bff36fd64c54e9fda5
Daniele Muffato <danixgithub1@gmail.com>
2023-11-07 00:06:24 +0100

    added init script for sway, and few changes in sway config file

--- init/swap.sh
+++ init/swap.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-while ! sudo sway -y; do
+while ! sudo sway sway-lock wl-clipboard python3-pip brightnessctl -y; do
     color "1;31" "installation of utilities failed!"
 done
+
+# necessary for autotiling python script
+pip install i3ipc
+
+# install autotiling pythong script
+cd ~/.personal/repos/other || exit 1
+git clone https://github.com/nwg-piotr/autotiling.git
+chmod +x ./autotiling/autotiling/main.py
+
+# necessary to change brightness (without sudo)
+sudo chmod +s $(which brightnessctl)
