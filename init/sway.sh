4c8e03fd0c871f9959429efe241e419452a330aa
Daniele Muffato <danixgithub1@gmail.com>
2023-11-07 11:37:36 +0100

    few sway fixes

--- init/sway.sh
+++ init/sway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! sudo apt install sway swaylock wl-clipboard python3-pip brightnessctl playerctl rofi grim slurp -y; do
+while ! sudo apt install sway swaylock wl-clipboard python3-pip brightnessctl playerctl rofi grim slurp waybar -y; do
     echo -e "\e[1;31minstallation of utilities failed!\e[m"
 done
 
@@ -16,4 +16,8 @@ git clone https://github.com/nwg-piotr/autotiling.git
 chmod +x ./autotiling/autotiling/main.py
 
 # install rofi themes
+cd $(mktemp -d) || exit 1
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi || exit 1
+chmod +x ./setup.sh; ./setup.sh
 rofi-theme-selector
