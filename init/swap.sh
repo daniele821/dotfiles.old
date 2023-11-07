cc8c326a1f50bdeed3f11615e118221953a84e7e
Daniele Muffato <danixgithub1@gmail.com>
2023-11-07 03:27:13 +0100

    schellcheck small fixes

--- init/swap.sh
+++ init/swap.sh
@@ -5,7 +5,7 @@ while ! sudo sway sway-lock wl-clipboard python3-pip brightnessctl playerctl rof
 done
 
 # necessary to change brightness (without sudo)
-sudo chmod +s $(which brightnessctl)
+sudo chmod +s "$(which brightnessctl)"
 
 # necessary for autotiling python script
 pip install i3ipc
@@ -16,7 +16,7 @@ git clone https://github.com/nwg-piotr/autotiling.git
 chmod +x ./autotiling/autotiling/main.py
 
 # install rofi themes
-git clone https://github.com/lr-tech/rofi-themes-collection.git 
+git clone https://github.com/lr-tech/rofi-themes-collection.git
 mkdir -p ~/.local/share/rofi/themes/
 cp rofi-themes-collection/themes/* ~/.local/share/rofi/themes/
 rofi-theme-selector
