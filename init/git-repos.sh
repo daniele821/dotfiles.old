85bd1e3cd0e1e6ce1af86a90a9459993623ad66d
Daniele Muffato <danixgithub1@gmail.com>
2023-11-12 01:27:47 +0100

    backported commit

--- init/git-repos.sh
+++ init/git-repos.sh
@@ -20,7 +20,7 @@ CODE_EXTENSIONS=(
 )
 
 # create directory structure
-mkdir -p ~/.personal/{repos/{mine,unibo,other,test},data}
+mkdir -p ~/.personal/{repos/{mine,unibo,other},data}
 
 # restore backup
 "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")/autosaver.sh" -by
